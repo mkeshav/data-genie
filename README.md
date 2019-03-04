51d8a27807001c558536b1f1612aa6e6bcfc688f
Keshav Murthy <keshav.murthy@latitudefinancial.com>
2019-03-05 09:08:44 +1100

    update setup py

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Data Genie
 
 Genie that can satisfy your data wish
+
+# Install
+python3 -m pip install --index-url https://test.pypi.org/simple/ data-genie-mkeshav
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="data-genie-keshav",
+    name="data-genie-mkeshav",
     version="0.0.2",
     author="Keshav Murthy",
     author_email="mkeshav@gmail.com",
