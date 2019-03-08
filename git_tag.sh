22411773f248f21fb8a888c2ad567d68ede6c20b
Keshav Murthy <keshav.murthy@latitudefinancial.com>
2019-03-08 14:40:32 +1100

    rename precommit hook.

--- git_tag.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-ver=$(python setup.py --version)
-echo "Tagging with version: $ver"
-git tag -a v$ver -m "Version $ver Release"
-echo "Pushing tags"
-git push origin --tags
