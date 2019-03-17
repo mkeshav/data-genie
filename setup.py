a2cbfd1deac850d74eb851bdfbc92957379642eb
Keshav Murthy <keshav.murthy@latitudefinancial.com>
2019-03-17 14:54:25 +1100

    increment version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="data-genie-mkeshav",
-    version="0.2.5",
+    version="0.2.6",
     author="Keshav Murthy",
     author_email="mkeshav@gmail.com",
     description="A small genie to generate test data",
