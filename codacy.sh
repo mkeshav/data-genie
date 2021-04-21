2b6ad594d424928d651b9faf4fa68a03e30150d7
Keshav Murthy <mkeshav@gmail.com>
2021-04-21 13:07:35 +1000

    Start uploading coverage to codacy instead of sonar

---
+++ codacy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -eu
+
+echo "Generate coverage and test report"
+python3 setup.py develop && pytest --cov=genie_pkg --cov-report=xml
+
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r coverage.xml \
+    --project-token $CODACY_PROJECT_TOKEN \
+    --organization-provider gh \
+    --username mkeshav \
+    --project-name data-genie
