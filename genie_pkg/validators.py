7a56d1887ca54d42f33722d3f77699b92a1b031c
Keshav Murthy <mkeshav@gmail.com>
2021-04-26 07:46:02 +1000

    Generate proper doc strings

--- genie_pkg/validators.py
+++ genie_pkg/validators.py
@@ -1,6 +1,14 @@
 from genie_pkg.generators import check_digit_luhn_mod_10
 
-def is_valid_cc(cc):
+def is_valid_cc(cc) -> bool:
+    """[summary]
+
+    Args:
+        cc ([str]): Credit card number
+
+    Returns:
+        [bool]: True if it is a valid cc
+    """
     first_15_digits = cc[:-1]
     check_digit = cc[len(cc) - 1]
     return check_digit_luhn_mod_10(first_15_digits) == int(check_digit)
