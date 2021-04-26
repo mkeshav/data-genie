52d849a4ae49c1cf87c576acda417843d34e56da
Keshav Murthy <mkeshav@gmail.com>
2021-04-26 15:59:19 +1000

    Increment version

--- genie_pkg/__init__.py
+++ genie_pkg/__init__.py
@@ -2,7 +2,7 @@
 
 """Genie that fulfils your data dreams.Look at https://data-genie.readthedocs.io for documentation."""
 
-__version__ = '1.3.4'
+__version__ = '1.3.5'
 
 class GenieException(Exception):
     pass
